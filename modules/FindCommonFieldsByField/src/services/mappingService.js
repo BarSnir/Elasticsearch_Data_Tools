c41b4c72d95f2b4c4e61c279e552d21ef0eee160
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-08-05 16:51:08 +0300

    parsed mappings

--- modules/FindCommonFieldsByField/src/services/mappingService.js
+++ modules/FindCommonFieldsByField/src/services/mappingService.js
@@ -1,12 +1,17 @@
+const objectPath = require('object-path-get');
 const server = require('../../server');
 
 module.exports = {
     async getIndexMapping() {
         const ecClient = server.getECConnection();
-        const indexMapping = await ecClient.indices.getMapping({
+        const response = await ecClient.indices.getMapping({
             index: process.env.ELASTICSEARCH_INDEX_NAME,
             include_type_name: process.env.ELASTICSEARCH_TYPE_INCLUDED,
         });
-        console.log(indexMapping);
-    }
+        console.log(objectPath(response, this.getMappingPath()));
+        return objectPath(response, this.getMappingPath());
+    },
+    getMappingPath(){
+        return `body.${process.env.ELASTICSEARCH_INDEX_NAME}.mappings.properties`;
+    },
 }
