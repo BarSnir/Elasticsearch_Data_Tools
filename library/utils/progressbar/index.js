8e00269a3a7c8184e513d3efc0131ec09d8f4105
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-08-05 09:56:47 +0300

    added ProgressBar module to utils

---
+++ library/utils/progressbar/index.js
@@ -0,0 +1,30 @@
+const cliProgress = require('cli-progress'); 
+const _colors = require('colors');
+
+module.exports = {
+    color: null,
+    message: null,
+    construct(color, message){
+      this.color = color;
+      this.message = message;
+    },
+    runProgressBar(){
+      let color = _colors[this.color];
+      return new cliProgress.SingleBar({
+        format: 'CLI Progress |' + color('{bar}') +  `| {percentage}% || {value}/{total} Documents 🍺 ${this.message}`,
+        barCompleteChar: '\u2588',
+        barIncompleteChar: '\u2591',
+        hideCursor: true
+      });
+    },
+    startBar(count) {
+        this.currentProgressBar = this.runProgressBar();
+        this.currentProgressBar.start(count, 0);
+    },
+    completeProcess(){
+        this.currentProgressBar.stop(); 
+    },
+    incrementProcess(int=1){
+        this.currentProgressBar.increment(int);
+    }
+}
