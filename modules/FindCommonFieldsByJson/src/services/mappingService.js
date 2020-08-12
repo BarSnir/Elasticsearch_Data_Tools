4840e74009ab0ea460884bf4e2a0d18f633afdc7
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-08-12 09:57:21 +0300

    organize .env path

--- modules/FindCommonFieldsByJson/src/services/mappingService.js
+++ modules/FindCommonFieldsByJson/src/services/mappingService.js
@@ -2,8 +2,7 @@ const fsUtil = require('../../../../library/utils/fs')
 
 module.exports = {
     getJson() {
-        console.log(this.getJsonPath());
-        //return fsUtil.getJsonSync(this.getJsonPath());
+        return fsUtil.getJsonSync(this.getJsonPath());
     },
     getJsonPath(){
         return `${process.env.PWD}/skeletons/${process.env.JSON_NAME}`;
