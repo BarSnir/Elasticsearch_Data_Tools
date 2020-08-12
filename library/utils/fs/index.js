9c838e07450a87ce0faa60ac55e2b08fa32ed6fa
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-08-12 10:51:37 +0300

    ignore jsons

--- library/utils/fs/index.js
+++ library/utils/fs/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = {
     getJsonSync(path){
-        return fs.readFileSync(path);
+        return JSON.parse(fs.readFileSync(path));
     },
     writeJson(fileName, payload){
         fs.appendFileSync(`./${fileName}.json`, JSON.stringify(payload, null ,2));
