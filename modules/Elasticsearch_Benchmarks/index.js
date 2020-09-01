5fcdd2c88f8b6790589f42f06733f8346686b36c
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-09-01 13:17:40 +0300

    adding new connector for logzio

--- modules/Elasticsearch_Benchmarks/index.js
+++ modules/Elasticsearch_Benchmarks/index.js
@@ -1,9 +1,9 @@
 const server = require('./src/server');
-const services = require('./src/services');
+const LogzioLogger = require('../../library/connectors/logzio');
 server.configEnv();
 server.configRepo();
 
 async function runModule() {
-    
+    LogzioLogger.sendLog("Benchmark application");
 }
 runModule()
--- modules/Elasticsearch_Benchmarks/src/server.js
+++ modules/Elasticsearch_Benchmarks/src/server.js
@@ -1,18 +1,23 @@
 const dotenv = require('dotenv');
 const path = require('path');
 const elasticRepo = require('./repositories/elasticsearchRepo');
+const logzioClient = require('../../../library/connectors/logzio')
 const logger = require('./../../../library/utils/logger');
 
 module.exports = {
-    env: "development",
+    env: 'development',
+    env_path: '../.env',
     logMessages:{
         a:`Connection establish by Elastic cloud.\n`,
+        b:`Logzio connector established`
     },
     configEnv(){
-        dotenv.config({path:path.resolve(__dirname, '../.env')});
+        dotenv.config({path:path.resolve(__dirname, this.env_path)});
     },
     configRepo(){
         elasticRepo.initializeECConnection();
-        logger.log(this.logMessages.a)
+        logger.log(this.logMessages.a);
+        logzioClient.initLogzioLogger();
+        logger.log(this.logMessages.b)
     }
 }
