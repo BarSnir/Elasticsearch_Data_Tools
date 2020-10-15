452b37c082f6b62d2f54863489f6abf7a0aa5292
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-10-15 13:15:29 +0300

    added another test to esBenchmark

---
+++ modules/Elasticsearch_Benchmarks/src/tests/transformers/reportTransformer.test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const expect = chai.expect;
+const reportTransformer = require('../../transformers/reportTransformer');
+const timeUtils = require('../../../library/utils/time');
+
+
+describe('transformers properties check', ()=>{
+    it('log.a is a string',()=>{
+        //assign
+        const logsMessage = reportTransformer.logMessages.a;
+        //assert
+        expect(logsMessage).to.be.a('string');
+    });
+})
+
